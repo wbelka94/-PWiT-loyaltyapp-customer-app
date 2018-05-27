6d18bc9f3a1bf52cd10831ba33349de7945426ea
wbelka94 <wbelka94@gmail.com>
2018-05-28 00:43:41 +0200

    TabNavigator + StackNavigator

--- App.js
+++ App.js
@@ -4,15 +4,14 @@ import MyPointsScreen from "./screens/MyPointsScreen";
 import CompanyScreen from "./screens/CompanyScreen";
 import createDrawerNavigator from "react-navigation/src/navigators/createDrawerNavigator";
 import CouponScreen from "./screens/CouponScreen";
+import QRScan from "./screens/QRScan";
+import {createBottomTabNavigator, createStackNavigator} from "react-navigation";
 
-const App = createDrawerNavigator ({
+
+const MyPoints = createStackNavigator({
     Home: {
         path: '/home',
-        screen: HomeScreen,
-    },
-    MyPoints: {
-         path: '/home/my-points',
-         screen: MyPointsScreen
+        screen: MyPointsScreen,
     },
     Company: {
         path: 'home/my-points/company',
@@ -21,13 +20,31 @@ const App = createDrawerNavigator ({
     Coupon: {
         path: 'home/my-points/company/coupon',
         screen: CouponScreen
-    }
+    },
 },{
     initialRouteName: 'Home',
     drawerPosition: 'right',
+    headerMode: 'none',
     navigationOptions: {
         headerMode: 'none',
+        headerVisible: false,
     }
 });
+
+const App = createBottomTabNavigator ({
+    Home: {
+        path: '/home',
+        screen: HomeScreen,
+    },
+    MyPoints: {
+         path: '/home/my-points',
+         screen: MyPoints
+    },
+    QRScan: {
+        screen: QRScan
+    }
+});
+
+
 
 export default App;
